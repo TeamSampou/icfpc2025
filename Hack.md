d86939a2fcf6f04376c6ad3647aa7b99f5a6450e
Nobuo Yamashita <nobsun@gmail.com>
2025-08-01 07:36:53 +0900

    start project

---
+++ Hack.md
@@ -0,0 +1,32 @@
+# Hack
+
+## Library など準備
+
+```
+sudo apt update
+sudo apt upgrade
+```
+
+ghcup 用
+```
+sudo apt install build-essential libffi-dev libgmp-dev libtinfo-dev
+```
+
+graphviz用
+```
+sudo apt install graphviz
+```
+
+gloss 用
+```
+sudo apt install freeglut3-dev libxi-dev libxcursor-dev libxinerama-dev libxxf86vm-dev
+```
+
+hmatrix 用
+```
+sudo apt install libgsl0-dev liblapack-dev libatlas-base-dev
+```
+
+
+
+
---
+++ app/Main.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE GHC2021 #-}
+{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LexicalNegation #-}
+{-# LANGUAGE LambdaCase, MultiWayIf #-}
+{-# LANGUAGE NPlusKPatterns #-}
+{-# LANGUAGE DataKinds, PolyKinds, NoStarIsType, TypeFamilyDependencies #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import System.Environment
+
+main :: IO ()
+main = do
+    { prog <- getProgName
+    ; args <- getArgs
+    ; putStrLn $ "program:   " ++ prog
+    ; putStrLn $ "arguments: " ++ unwords args
+    } 

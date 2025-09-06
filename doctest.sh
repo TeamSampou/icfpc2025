5621f44ea0f2ee91314c388d80c39d21335e8ea0
Kei Hibino <ex8k.hibino@gmail.com>
2025-09-06 10:18:38 +0900

    update doctest.sh

--- doctest.sh
+++ doctest.sh
@@ -1,4 +1,19 @@
 #! /bin/sh
 
-find src -type f | \
-    xargs -n 1 doctest
+has_doctest() {
+    egrep -q '^( *|-- )>>> ' "$1" > /dev/null
+}
+
+vecho=:
+if [ x"$1" = x-v -o x"$1" = x--verbose ]; then
+    vecho=echo
+fi
+
+find src -type f -name '*.hs' | while read path ; do
+    if has_doctest "$path" ; then
+        $vecho "* run doctest for $path"
+        doctest $path
+    else
+        $vecho "* doctest not found for $path"
+    fi
+done

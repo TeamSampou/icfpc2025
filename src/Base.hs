00d2d6cbbe1cea28752cc72f97beb2a389b71293
Masahiro Sakai <masahiro.sakai@gmail.com>
2025-09-06 16:16:25 +0900

    add maxPlan and randomWalk

--- src/Base.hs
+++ src/Base.hs
@@ -6,6 +6,10 @@ module Base
   , Plan
   ) where
 
+import Control.Monad
+import Control.Monad.Primitive
+import qualified System.Random.MWC as Rand
+
 type Layout = ([RoomLabel], RoomIndex, [((RoomIndex, Door), (RoomIndex, Door))])
 
 -- | 2-bit room labels
@@ -16,3 +20,9 @@ type RoomIndex = Int
 type Door = Int
 
 type Plan = String
+
+maxPlan :: Int -> Int
+maxPlan numRooms = numRooms * 18
+
+randomWalk :: PrimMonad m => Rand.Gen (PrimState m) -> Int -> m Plan
+randomWalk gen steps = fmap (concat . map show) $ replicateM steps (Rand.uniformR (0::Door, 5) gen)
